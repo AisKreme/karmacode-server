d0962910ede4b701b39a0043d42985f6ef6e222c
Maik Jüngling <juengling.maik@gmail.com>
2021-12-10 12:21:05 +0100

    only one Organisation middleware

---
+++ middleware/hasOrganisation.js
@@ -0,0 +1,13 @@
+const hasOrganisation = (req, res, next) => {
+  const { organisation } = req.session.keks;
+  console.log("Check:", req.session.keks);
+  if (!organisation) {
+    next();
+  } else {
+    res
+      .status(401)
+      .json({ message: "Only one organisation possible to create." });
+  }
+};
+
+module.exports = hasOrganisation;
--- routes/organisation.routes.js
+++ routes/organisation.routes.js
@@ -4,76 +4,82 @@ const bcrypt = require("bcryptjs");
 const Organisation = require("../models/Organisation.model");
 const UserModel = require("../models/User.model");
 const isLoggedIn = require("../middleware/isLoggedIn");
+const hasOrganisation = require("../middleware/hasOrganisation");
 
-router.post("/create-organisation", isLoggedIn, async (req, res) => {
-  const { _id } = req.session.keks;
+router.post(
+  "/create-organisation",
+  isLoggedIn,
+  hasOrganisation,
+  async (req, res) => {
+    const { _id } = req.session.keks;
 
-  const { name, street, houseNr, zip, city, country, description, image } =
-    req.body;
+    const { name, street, houseNr, zip, city, country, description, image } =
+      req.body;
 
-  let contact = {
-    user: _id,
-    links: [],
-  };
+    let contact = {
+      user: _id,
+      links: [],
+    };
 
-  let streetData = `street=${houseNr}+${street},&city=${city},&country=${country},&postalcode=${zip}`;
-  try {
-    const { data } = await axios.get(
-      `https://nominatim.openstreetmap.org/search?${streetData}&format=geocodejson`
-    );
-    const longitude = data.features[0].geometry.coordinates[0];
-    const latitude = data.features[0].geometry.coordinates[1];
+    let streetData = `street=${houseNr}+${street},&city=${city},&country=${country},&postalcode=${zip}`;
+    try {
+      const { data } = await axios.get(
+        `https://nominatim.openstreetmap.org/search?${streetData}&format=geocodejson`
+      );
+      const longitude = data.features[0].geometry.coordinates[0];
+      const latitude = data.features[0].geometry.coordinates[1];
 
-    if (!name) {
-      res
-        .status(500)
-        .json({ error: "Please enter a name for your organisation." });
+      if (!name) {
+        res
+          .status(500)
+          .json({ error: "Please enter a name for your organisation." });
+      }
+      if (
+        !street ||
+        !houseNr ||
+        !zip ||
+        !city ||
+        !country ||
+        !longitude ||
+        !latitude
+      ) {
+        res
+          .status(500)
+          .json({ error: "Please make sure to enter a valid address." });
+      }
+
+      const organisation = await Organisation.create({
+        name,
+        street,
+        houseNr,
+        zip,
+        city,
+        country,
+        latitude,
+        longitude,
+        description,
+        image,
+        contact,
+      });
+
+      const user = await UserModel.findByIdAndUpdate(
+        _id,
+        {
+          organisation: organisation._id,
+        },
+        { new: true }
+      );
+      user.password = "***";
+      req.session.keks = user;
+      res.status(200).json(organisation);
+    } catch (err) {
+      res.status(500).json({
+        errorMessage: "Something went wrong! Go to sleep!",
+        message: err,
+      });
     }
-    if (
-      !street ||
-      !houseNr ||
-      !zip ||
-      !city ||
-      !country ||
-      !longitude ||
-      !latitude
-    ) {
-      res
-        .status(500)
-        .json({ error: "Please make sure to enter a valid address." });
-    }
-
-    const organisation = await Organisation.create({
-      name,
-      street,
-      houseNr,
-      zip,
-      city,
-      country,
-      latitude,
-      longitude,
-      description,
-      image,
-      contact,
-    });
-
-    const user = await UserModel.findByIdAndUpdate(
-      _id,
-      {
-        organisation: organisation._id,
-      },
-      { new: true }
-    );
-    user.password = "***";
-    req.session.keks = user;
-    res.status(200).json(organisation);
-  } catch (err) {
-    res.status(500).json({
-      errorMessage: "Something went wrong! Go to sleep!",
-      message: err,
-    });
   }
-});
+);
 
 router.patch("/edit-organisation", isLoggedIn, async (req, res) => {
   const { organisation: organisationId } = req.session.keks;
@@ -164,6 +170,8 @@ router.delete("/organisation/delete", isLoggedIn, async (req, res) => {
       { new: true }
     );
     await Organisation.findByIdAndDelete({ _id: user.organisation });
+    user.password = "***";
+    req.session.keks = user;
     res.status(204).json({ message: "Organisation deleted" });
   } catch (err) {
     res.status(500).json({
